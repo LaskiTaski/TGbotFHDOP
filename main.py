388c80632c13c4a4d98bb7789c1229db721ba3e9
Tim <dgj.vap.11@mail.ru>
2024-10-31 18:16:32 +0300

    Create new project

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from aiogram.utils import executor
+# from bot_telegram import dp
+
+
+
+
+async def on_start_up(_):
+    pass
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_start_up)
 
